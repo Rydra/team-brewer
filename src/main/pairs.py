5cea9ac852d5a0aeb8004f7611a7f36c8d8ed7ac
David Jiménez <davigetto@gmail.com>
2017-10-26 22:56:12 +0200

    Refactor

--- src/main/pairs.py
+++ src/main/pairs.py
@@ -10,13 +10,13 @@ import re
 
 
 def load_already_generated_pairs(lines):
-    pairs = defaultdict(set)
+    history_dates_per_user = defaultdict(set)
     for line in lines:
         names = [x.strip() for x in line.split(',')]
 
-        pairs[names[0]].add(names[1])
+        history_dates_per_user[names[0]].add(names[1])
 
-    return pairs
+    return history_dates_per_user
 
 
 def have_already_met(mate1, mate2, already_generated_pairs):
@@ -24,7 +24,7 @@ def have_already_met(mate1, mate2, already_generated_pairs):
 
 
 def pop_next_candidate(people_list, last_alone):
-    if last_alone :
+    if last_alone:
         mate1 = last_alone
         last_alone = None
         people_list.remove(mate1)
@@ -92,7 +92,6 @@ def generate_pairs_for_speeddater(speed_dater, num_dates, people, pairs_already_
     people.remove(speed_dater)
 
     for _ in range(num_dates):
-
         mate2 = pop_next_partner(speed_dater, people, pairs_already_generated)
 
         generated_pair = (speed_dater, mate2)
@@ -104,90 +103,118 @@ def generate_pairs_for_speeddater(speed_dater, num_dates, people, pairs_already_
     return pairs
 
 
-def main():
-    parser = argparse.ArgumentParser(description='Parse arguments for debugging purposes')
-    parser.add_argument('inputfilename', help='the path to the list of names')
-    parser.add_argument('outputfilename', help='the file to store the pairs that have been already generated')
-    parser.add_argument('--speeddate', type=str, help='Prepare first a speed dating with this user')
-
-    parsed_arguments = parser.parse_args()
-
-    abspath = os.path.abspath(__file__)
-    dname = os.path.dirname(abspath)
-    inputfilename = os.path.join(dname, parsed_arguments.inputfilename)
-    outputfilename = os.path.join(dname, parsed_arguments.outputfilename)
-
-    last_alone = None
-    already_generated = defaultdict(set)
-
-    with open(inputfilename, "r", encoding="utf-8") as inputfs:
+def get_people(filename):
+    with open(filename, "r", encoding="utf-8") as inputfs:
         people = [x.strip() for x in inputfs.readlines() if x and not x.startswith('#')]
 
-    if os.path.exists(outputfilename):
-        with open(outputfilename, "r", encoding="utf-8") as outputfs:
+    return people
+
+
+def get_history_of_dates(filename):
+    last_alone_person = None
+    history_dates_per_user = defaultdict(set)
+
+    if os.path.exists(filename):
+        with open(filename, "r", encoding="utf-8") as outputfs:
             lines = [x.strip() for x in outputfs.readlines() if x]
 
             if lines and 'last_alone:' in lines[0]:
-                last_alone = re.findall('last_alone:(.*)', lines.pop(0))[0].strip()
+                last_alone_person = re.findall('last_alone:(.*)', lines.pop(0))[0].strip()
 
-            already_generated = load_already_generated_pairs(lines)
+            history_dates_per_user = load_already_generated_pairs(lines)
 
-    pairs = []
+    return history_dates_per_user, last_alone_person
 
-    # As pairs are being generated, people are removed from this queue
-    people_queue = list(people)
 
-    if parsed_arguments.speeddate:
-        pairs_speeddate = generate_pairs_for_speeddater(parsed_arguments.speeddate, 3, people_queue, already_generated)
-        pairs.extend(pairs_speeddate)
-
-    pairs_2, lone_wolf = generate_pairs(people_queue,
-                           already_generated,
-                           last_alone if last_alone != parsed_arguments.speeddate else None,
-                            people)
-
-    pairs.extend(pairs_2)
-
-    new_last_alone = None
-
-    last_pair = pairs[-1]
-    cool_mate = None
-    if lone_wolf:
-        new_last_alone = last_pair[0]
-        cool_mate = last_pair[1]
-
-    content = f"Hi buttoners, how's it going? These are the dates that have been arranged " \
-              f"for this week (starting at {datetime.datetime.now().strftime('%Y-%m-%d')}) \n"
-    for mate1, mate2 in pairs:
-        content += f'{mate1} with {mate2}\n'
-
-    content += '\n'
-
-    # TODO: Instead of being alone, someone will have two dates
-    if new_last_alone:
-        content += f'{cool_mate}, since we are an odd number of people, we randomly matched you with {lone_wolf}, ' \
-                   f'so you will have two dates this week :D'
-
-    print(content)
-
-    # Slack test
-    # https://api.slack.com/methods/chat.postMessage#channels
-
+def notify_slack(message):
     slack_token = os.environ["SLACK_API_TOKEN"]
     sc = SlackClient(slack_token)
 
     sc.api_call(
         "chat.postMessage",
         channel="#team-brewer",
-        text=f"@everyone\n```{content}```",
+        text=f"@everyone\n```{message}```",
         as_user=True
     )
 
-    with open(outputfilename, "w", encoding="utf-8") as outputfs:
-        if new_last_alone:
-            outputfs.write(f'last_alone:{new_last_alone}\n')
 
-        for mate1, mates in already_generated.items():
+def generate_matches_message(pairs, lone_wolf):
+    content = f"Hi buttoners, how's it going? These are the dates that have been arranged " \
+              f"for this week (starting at {datetime.datetime.now().strftime('%Y-%m-%d')}) \n"
+
+    for mate1, mate2 in pairs:
+        content += f'{mate1} with {mate2}\n'
+
+    content += '\n'
+
+    # TODO: Instead of being alone, someone will have two dates
+    if lone_wolf:
+        cool_mate = None
+        if lone_wolf:
+            last_pair = pairs[-1]
+            cool_mate = last_pair[1]
+
+        content += f'{cool_mate}, since we are an odd number of people, we randomly matched you with {lone_wolf}, ' \
+                   f'so you will have two dates this week :D'
+
+    return content
+
+
+def main():
+    parser = argparse.ArgumentParser(description='Parse arguments for debugging purposes')
+    parser.add_argument('inputfilename', help='the path to the list of names')
+    parser.add_argument('outputfilename', help='the file to store the pairs that have been already generated')
+    parser.add_argument('--speeddater', type=str, help='Prepare this person for some speed dates')
+
+    parsed_arguments = parser.parse_args()
+
+    abspath = os.path.abspath(__file__)
+    dname = os.path.dirname(abspath)
+
+    inputfilename = os.path.join(dname, parsed_arguments.inputfilename)
+    outputfilename = os.path.join(dname, parsed_arguments.outputfilename)
+    speed_dater = parsed_arguments.speeddater
+
+    people = get_people(inputfilename)
+
+    # The last alone person is the person we are going to prioritize
+    history_dates_per_user, last_alone_person = get_history_of_dates(outputfilename)
+
+    if last_alone_person == speed_dater:
+        last_alone_person = None
+
+    pairs = []
+
+    # As pairs are being generated, people are removed from this queue
+    people_queue = list(people)
+
+    if speed_dater:
+        pairs_speeddate = generate_pairs_for_speeddater(speed_dater,
+                                                        3,
+                                                        people_queue,
+                                                        history_dates_per_user)
+        pairs.extend(pairs_speeddate)
+
+    pairs_2, lone_wolf = generate_pairs(people_queue,
+                                        history_dates_per_user,
+                                        last_alone_person,
+                                        people)
+
+    pairs.extend(pairs_2)
+
+    content = generate_matches_message(pairs, lone_wolf)
+
+    print(content)
+
+    # Slack test
+    # https://api.slack.com/methods/chat.postMessage#channels
+    notify_slack(content)
+
+    with open(outputfilename, "w", encoding="utf-8") as outputfs:
+        if lone_wolf:
+            outputfs.write(f'last_alone:{lone_wolf}\n')
+
+        for mate1, mates in history_dates_per_user.items():
             for mate in mates:
                 outputfs.write(f'{mate1},{mate}\n')
 
